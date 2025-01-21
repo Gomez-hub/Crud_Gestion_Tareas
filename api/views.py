39507b48a76372c20bf4f38b2735fba0072b98f9
Miguel Gomez <mg0847091@gmail.com>
2025-01-21 10:45:51 -0500

    Cambio en las vistas

--- api/views.py
+++ api/views.py
@@ -1,10 +1,39 @@
-from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from django.http import HttpResponse
 from .models import Tareas
 from .serializer import TareasSerializer
 
 
-class TareasViewSet(viewsets.ModelViewSet):
-    queryset = Tareas.objects.all()
-    serializer_class = TareasSerializer
+class TareasViewSet(APIView):
+    def get(self, request):
+        tareas = Tareas.objects.all()
+        serializer = TareasSerializer(tareas, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = TareasSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def put(self, request, pk):
+        tarea = Tareas.objects.get(id=pk)
+        serializer = TareasSerializer(tarea, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        tarea = Tareas.objects.get(id=pk)
+        tarea.delete()
+        return HttpResponse('Tarea eliminada')
+
+
+# class TareasViewSet(viewsets.ModelViewSet):
+#     queryset = Tareas.objects.all()
+#     #ghjkf
+#     serializer_class = TareasSerializer
 
